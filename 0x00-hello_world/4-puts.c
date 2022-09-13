c1fed99b7cc695ee092a20725e9d0127e0cdb93b
SilverT28 <silverteria@gmail.com>
2022-09-13 02:09:13 +0000

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - program execution starts here
+ * Description: this is the main function
+ * Return: an integer is returned to signify success or failure
+ */
 int main(void)
 {
-	puts("Programming is like builing a multilingual puzzle");
+	puts("\"Programming is like builing a multilingual puzzle");
 	return (0);
-}	
+}
