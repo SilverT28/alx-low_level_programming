af621d659fc7703056af2e10504852471aea2d5b
SilverT28 <silverteria@gmail.com>
2022-09-13 03:14:37 +0000

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - where execution begins
+ * Return: print text or return one
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	59, 1, stderr);
+	return (1);
+}
