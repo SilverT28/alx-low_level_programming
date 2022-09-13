48f711cd6bf5c2e7108638eea1ac64fae000e402
SilverT28 <silverteria@gmail.com>
2022-09-13 02:21:10 +0000

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is where execution starts
+ * text: what should be printed
+ * return: print the text or fail
+ */ 
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);	
+}	
