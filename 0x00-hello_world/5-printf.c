52b5ad42ce0734af2ac441b5fa6b7096cd4ceea5
SilverT28 <silverteria@gmail.com>
2022-09-13 02:31:14 +0000

    trailing spaces removed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,9 @@
  * main - this is where execution starts
  * text: what should be printed
  * Return: print the text or fail
- */ 
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);	
+	return (0);
 }
