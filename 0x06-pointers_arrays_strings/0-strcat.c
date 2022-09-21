861144e3727b13ce20c6dfa3d1c02c08b58fe455
SilverT28 <silverteria@gmail.com>
2022-09-21 22:42:38 +0000

    added header file and task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = j = 0;
+	while (dest[i] != '\0') /* find end of dest */
+		i++;
+	while ((dest[i++] = src[j++]) != '\0') /* copy src */
+		;
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+More alx tasks on pointers, arrays and strings
